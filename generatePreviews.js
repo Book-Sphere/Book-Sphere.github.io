be3dbb3dd175a3831b1313a7000de330cfaf8197
BookSphere <books.era786@gmail.com>
2025-04-17 00:29:38 +0500

    redirect

--- generatePreviews.js
+++ generatePreviews.js
@@ -21,7 +21,7 @@ books.forEach((book) => {
   <meta property="og:description" content="By ${book.author}" />
   <meta property="og:image" content="${book.cover}" />
   <meta property="og:url" content="${yourDomain}/previews/book-${book.id}.html" />
-  <meta http-equiv="refresh" content="0; URL=../download.html?id=${book.id}" />
+  <meta http-equiv="refresh" content="0; URL=../download.html?bookId=${book.id}" />
 </head>
 <body>
   <p>Redirecting to download page...</p>
